859e5ebcc639e2272949c56966b35570b6540a71
Arghay013 <arghay.cse8.bu@gmail.com>
2024-08-26 12:15:53 +0600

    first

---
+++ prothom-ptothom/src/App.jsx
@@ -0,0 +1,13 @@
+import Cha from "./Cha/Cha"
+
+function App() {
+
+  return (
+    <>
+      <h1>Prothom prothom kaj kori</h1>
+      <Cha></Cha>
+    </>
+  )
+}
+
+export default App
---
+++ prothom-ptothom/src/Cha/Cha.jsx
@@ -0,0 +1,10 @@
+
+const Cha = () => {
+    return (
+        <>
+          <h1>Cha and react</h1>  
+        </>
+    );
+};
+
+export default Cha;
