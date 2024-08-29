76e50eba253b3d2c02ed8f514c22573294aa0407
Arghay013 <arghay.cse8.bu@gmail.com>
2024-08-29 19:30:28 +0600

    3rd

---
+++ 03-tailwind-props/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='text-2xl bg-purple-700 rounded-lg p-8'>Adp with REACT</h1> */}
+      <Card title="juta" />
+      <Card title="ami ja dibo setai" />
+    </>
+  )
+}
+
+export default App
